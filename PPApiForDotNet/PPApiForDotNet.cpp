8d0614a4c0ffe59688c338cd66304d468e095cc9
Andrew Arnott <andarno@microsoft.com>
2015-10-27 16:36:02 -0700

    Add sample C++ pepper plugin that builds

--- PPApiForDotNet/PPApiForDotNet.cpp
+++ PPApiForDotNet/PPApiForDotNet.cpp
@@ -4,3 +4,40 @@
 
 #include "PPApiForDotNet.h"
 
+#include "ppapi/cpp/instance.h"
+#include "ppapi/cpp/module.h"
+
+// This is the simplest possible C++ Pepper plugin that does nothing.
+
+// This object represents one time the page says <embed>.
+class MyInstance : public pp::Instance {
+public:
+    explicit MyInstance(PP_Instance instance) : pp::Instance(instance) {}
+    virtual ~MyInstance() {}
+
+    virtual bool Init(uint32_t argc, const char* argn[], const char* argv[]) {
+        return true;
+    }
+};
+
+// This object is the global object representing this plugin library as long
+// as it is loaded.
+class MyModule : public pp::Module {
+public:
+    MyModule() : pp::Module() {}
+    virtual ~MyModule() {}
+
+    // Override CreateInstance to create your customized Instance object.
+    virtual pp::Instance* CreateInstance(PP_Instance instance) {
+        return new MyInstance(instance);
+    }
+};
+
+namespace pp {
+
+    // Factory function for your specialization of the Module object.
+    Module* CreateModule() {
+        return new MyModule();
+    }
+
+}  // namespace pp
