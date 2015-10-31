6e5a4dbbe55dfc9a9f47e5d4e5c9120e426c6fb8
Andrew Arnott <andrew.arnott@microsoft.com>
2015-10-30 17:16:05 -0700

    Create a delegate to a managed method
    This fails in type loading.

--- src/CoreCLRHost/Main.cpp
+++ src/CoreCLRHost/Main.cpp
@@ -4,12 +4,21 @@
 #include "stdafx.h"
 #include "C:\git\PPApiForDotNet\src\PPApiForDotNet\CoreCLRHost.h"
 
+typedef int(__stdcall *Fn_entrypoint)(
+    const wchar_t* arg);
+
 int main()
 {
     HRESULT hr = StartClrHost();
-    if (FAILED(hr)) {
-        return 1;
-    }
+    if (FAILED(hr)) return 1;
+    Fn_entrypoint del;
+    hr = CreateManagedDelegate(
+        L"PPApiInCSharp, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null",
+        L"Program",
+        L"Main",
+        (void**)&del);
+    if (FAILED(hr)) return 2;
+    int result = del(L"Hello world!");
 
     return 0;
 }
